9b68d864a5b0e238291f867d5563b0459f3ea392
anders <anders@blomqvist.nu>
2012-10-22 12:12:39 +0200

    added mail.php

---
+++ mail.php
@@ -0,0 +1,26 @@
+<?php include ('includes/header.php'); 
+require ('includes/functions.php'); ?>
+
+	<div id="content">
+
+		<?php
+		$start_date = new DateTime('2012-10-22 14:10:58');
+		$now_diff = $start_date->diff(new DateTime('now'));
+		echo $now_diff->days.' days total<br>';
+		echo $now_diff->y.' years<br>';
+		echo $now_diff->m.' months<br>';
+		echo $now_diff->d.' days<br>';
+		echo $now_diff->h.' hours<br>';
+		echo $now_diff->i.' minutes<br>';
+		echo $now_diff->s.' seconds<br>';
+		
+		$result = get_zero();
+		foreach ($result as $id => $value) {
+			echo $value[0] ;
+		}
+		
+
+		?>
+	</div> <!-- end #content -->
+
+<?php include ('includes/footer.php') ?>
